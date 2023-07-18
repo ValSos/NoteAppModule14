b9d54f96793f5f5a179afa0b0ca50cbcc435300b
Valeriia Sosiedka <lera.dovgan.96@gmail.com>
2023-07-18 20:37:51 +0300

    Commit 3

--- src/main/java/com/example/NoteAppModule14Application.java
+++ src/main/java/com/example/NoteAppModule14Application.java
@@ -8,7 +8,9 @@ public class NoteAppModule14Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NoteAppModule14Application.class, args);
-
+//		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//		String encodedPassword = passwordEncoder.encode("default");
+//		System.out.println(encodedPassword);
 	}
 
 
---
+++ src/main/resources/db/migration/V4__populate_users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (username, password, role, enabled)
+VALUES
+    ('user', '$2a$10$nRWWXhRO0pmHNEUFWuMYgO5ZnnDEzL2f8GYp302aBuWaPb3z1K9E6', 'ROLE_USER', 1);
